ee445558e679b910eb2a91b38b4aa10d5de17d27
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 15:34:55 +0300

    s/import/include/ (C) whatthecommit.com

--- bin/local-rollback.sh
+++ bin/local-rollback.sh
@@ -6,5 +6,7 @@ ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
 git fetch --all -p -a --tags
+## FInally go to repo releases and remove last created tag, then:
 #git tag -l
 #git tag -d $latestTag
+#git fetch --all -p -a --tags
