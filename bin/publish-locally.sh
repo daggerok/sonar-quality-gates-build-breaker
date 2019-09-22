bb3b2e4f3c41047f426347308953c0468a1c8e3c
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 06:05:09 +0300

    Fix publish locally | This is not the commit message you are looking for (C) whatthecommit.com

--- bin/publish-locally.sh
+++ bin/publish-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ###
-# bash deploy-locally.sh
+# bash ./bin/publish-locally.sh
 ###
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-cd "${ROOT_PROJECT_DIR}" && ./mvnw -P deploy-locally
+cd "${ROOT_PROJECT_DIR}" && ./mvnw -P publish-locally
 #./mvnw release:clean release:prepare release:perform \
