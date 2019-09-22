2948765e142432cd63527fe5fa5951aba104fcd5
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 23:14:51 +0300

    I CAN HAZ COMMENTZ. (C) whatthecommit.com

---
+++ bin/bintray-all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+###
+# bash ./bin/local-publish.sh
+###
+ARGS=${1:-}
+ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+cd "${ROOT_PROJECT_DIR}" && ./mvnw release:prepare release:perform -B -s .mvn/settings.xml \
+  -DskipTests -Darguments="-DskipTests" \
+  -DtagNameFormat="@{project.version}" \
+  -DautoVersionSubmodules=true \
+  -DgenerateReleasePoms=false \
+  -DpreparationGoals="clean" \
+  -DcompletionGoals="clean install" \
+  -Dresume=false
+#cd "${ROOT_PROJECT_DIR}" && ./mvnw install -DskipTests
