76e9fb4a2af2be845ad6207302d81f237e23f0a8
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 06:27:25 +0300

    totally more readable (C) whatthecommit.com

--- bin/local-publish.sh
+++ bin/local-publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ###
-# bash ./bin/publish-locally.sh
+# bash ./bin/local-publish.sh
 ###
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-cd "${ROOT_PROJECT_DIR}" && ./mvnw -P publish-locally
+cd "${ROOT_PROJECT_DIR}" && ./mvnw -P local-publish
 #./mvnw release:clean release:prepare release:perform \
--- bin/local-release.sh
+++ bin/local-release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ###
-# bash ./bin/publish-locally.sh
+# bash ./bin/local-release.sh
 ###
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw -DskipTests -B -s .mvn/settings.xml \
-  -Darguments="-Dmaven.deploy.skip=true -Prelease-locally,publish-locally" \
+  -Darguments="-Dmaven.deploy.skip=true -Plocal-release,local-publish" \
   -Dresume=false release:prepare release:perform \
-  -Prelease-locally,publish-locally
+  -Plocal-release,local-publish
 #cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
