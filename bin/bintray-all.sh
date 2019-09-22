fc1c1674d8dfd43cf046a90e235f734353dfe15a
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 23:52:46 +0300

    Argh! About to give up :( (C) whatthecommit.com

--- bin/bintray-all.sh
+++ bin/bintray-all.sh
@@ -5,11 +5,6 @@
 ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw release:prepare release:perform -B -s .mvn/settings.xml \
-  -DskipTests -Darguments="-DskipTests" \
-  -DtagNameFormat="@{project.version}" \
-  -DautoVersionSubmodules=true \
-  -DgenerateReleasePoms=false \
-  -DpreparationGoals="clean" \
-  -DcompletionGoals="clean install" \
-  -Dresume=false
-#cd "${ROOT_PROJECT_DIR}" && ./mvnw install -DskipTests
+  -DskipTests -Darguments="-DskipTests" -DtagNameFormat="@{project.version}" \
+  -Dresume=false -DautoVersionSubmodules=true -DgenerateReleasePoms=false \
+  -DpreparationGoals="clean" -DcompletionGoals="clean install"
