73d9f22b788c833497df992ac0cee56dd57c44f1
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 15:55:47 +0300

    We'll figure it out on Monday (C) whatthecommit.com

--- bin/local-release.sh
+++ bin/local-release.sh
@@ -6,8 +6,7 @@ ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw -DskipTests -B -s .mvn/settings.xml \
   -Dresume=false release:prepare release:perform \
-  -Darguments="-DskipTests" \
   -Plocal-release ${ARGS}
 #cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
 git fetch --all -p -a --tags
-cd "${ROOT_PROJECT_DIR}" && ./mvnw install -DskipTests
+cd "${ROOT_PROJECT_DIR}" && ./mvnw -DskipTests install
