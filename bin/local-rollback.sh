5a58d5473d222c91152ddf863316adfa0adae88b
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 15:30:39 +0300

    these confounded tests drive me nuts (C) whatthecommit.com

---
+++ bin/local-rollback.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+###
+# bash ./bin/local-rollback.sh
+###
+ARGS=${1:-}
+ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
+git fetch --all -p -a --tags
