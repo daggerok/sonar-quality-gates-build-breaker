d2c1868775df448dd451c4d525ef357f7b8c5e9c
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 21:16:44 +0300

    Not one conflict, today was a good day. (C) whatthecommit.com

--- bin/github-all.sh
+++ bin/github-all.sh
@@ -5,3 +5,4 @@
 ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && bash ./bin/local-release.sh && bash ./bin/local-publish.sh && bash ./bin/github-publish.sh
+cd "${ROOT_PROJECT_DIR}" && git fetch --all -p -a --tags
