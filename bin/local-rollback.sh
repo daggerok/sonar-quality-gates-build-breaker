6262d77fe94609acb97a9d306358dc83b84738c4
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 15:32:01 +0300

    This is a basic implementation that works. (C) whatthecommit.com

--- bin/local-rollback.sh
+++ bin/local-rollback.sh
@@ -6,3 +6,5 @@ ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
 git fetch --all -p -a --tags
+#git tag -l
+#git tag -d $latestTag
