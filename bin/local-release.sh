aa073ef578ca1dde6efc4cbeecc6e5dacbab8102
Maksim Kostromin <daggerok@gmail.com>
2019-09-22 15:25:27 +0300

    Herping the fucking derp right here and now. (C) whatthecommit.com

--- bin/local-release.sh
+++ bin/local-release.sh
@@ -5,6 +5,7 @@
 ARGS=${1:-}
 ROOT_PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd "${ROOT_PROJECT_DIR}" && ./mvnw -DskipTests -B -s .mvn/settings.xml \
+  -Darguments="-Dmaven.deploy.skip=true -Plocal-release,local-publish" \
   -Dresume=false release:prepare release:perform \
   -Plocal-release ${ARGS}
 #cd "${ROOT_PROJECT_DIR}" && ./mvnw release:rollback -s .mvn/settings.xml
